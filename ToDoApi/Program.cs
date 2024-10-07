7362ac7a46d120c02eff6d2fea76a3295f4d3096
Jose Alves de Gouveia <gouveia32@gmail.com>
2024-10-07 15:40:24 -0300

    first commit

---
+++ ToDoApi/Program.cs
@@ -0,0 +1,14 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddGraphQLServer();
+
+var app = builder.Build();
+
+app.MapGraphQL();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
